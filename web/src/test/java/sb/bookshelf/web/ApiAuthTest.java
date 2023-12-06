1b6d3d143f7dbaec1b6f2c5da3fd8ec06d69eb92
Sergey Bochkov <bochkovsa@gmail.com>
2023-12-06 20:05:19 +0500

    update deps

--- web/src/test/java/sb/bookshelf/web/ApiAuthTest.java
+++ web/src/test/java/sb/bookshelf/web/ApiAuthTest.java
@@ -3,7 +3,6 @@ package sb.bookshelf.web;
 import java.util.Collections;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
