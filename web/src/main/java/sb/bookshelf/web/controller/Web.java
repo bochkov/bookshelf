545153f7a93f8e199cfbff31f259543aa970c7f2
Sergey Bochkov <bochkovsa@gmail.com>
2023-12-10 14:07:12 +0500

    update web model

--- web/src/main/java/sb/bookshelf/web/controller/Web.java
+++ web/src/main/java/sb/bookshelf/web/controller/Web.java
@@ -1,10 +1,14 @@
 package sb.bookshelf.web.controller;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sb.bookshelf.common.model.VolumeInfo;
+import sb.bookshelf.web.model.Volume;
 import sb.bookshelf.web.service.VolumeService;
 
 @Slf4j
@@ -17,22 +21,30 @@ public class Web {
 
     @GetMapping("/")
     public String index(Model model) {
+        List<VolumeInfo> vols = volumes.latest(20)
+                .stream()
+                .map(Volume::toVolumeInfo)
+                .toList();
         model.addAttribute("title", "Latest titles");
-        model.addAttribute("volumes", volumes.latest(10));
+        model.addAttribute("volumes", vols);
         return "index";
     }
 
     @PostMapping("/search")
     public String search(@RequestParam String query, Model model) {
+        List<VolumeInfo> vols = volumes.find(query)
+                .stream()
+                .map(Volume::toVolumeInfo)
+                .toList();
         model.addAttribute("title", String.format("Search for '%s'", query));
         model.addAttribute("query", query);
-        model.addAttribute("volumes", volumes.find(query));
+        model.addAttribute("volumes", vols);
         return "index";
     }
 
     @GetMapping("/v/{id}")
     public String details(@PathVariable String id, Model model) {
-        model.addAttribute("volume", volumes.get(id));
+        model.addAttribute("volume", volumes.get(id).toVolumeInfo());
         return "details";
     }
 
